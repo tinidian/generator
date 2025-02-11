807483341c78a97b3af16be9184c933516d7a5d8
tinidian <tinidian@gmail.com>
2025-02-11 08:57:32 -0700

    Shell scripts

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+import os
+import shutil
 from textnode import TextNode, TextType
+
+if os.path.exists("public"):
+    os.rmdir("public")
+os.mkdir("public")
+shutil.copytree("src", "public")
+
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
-
-
 
 main()
